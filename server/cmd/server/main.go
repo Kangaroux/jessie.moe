1b11f6e934f0756879ddeac3038d83ac46d3abd7
Jessie <2302541+Kangaroux@users.noreply.github.com>
2021-05-14 22:08:08 -0400

    Move some server files around, add request log middleware

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -5,11 +5,11 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/kangaroux/jessie_moe"
+	server "github.com/kangaroux/jessie_moe/internal"
 )
 
 func main() {
-	r := jessie_moe.NewRouter()
+	r := server.NewRouter()
 
 	fmt.Println("listening on :8080")
 	err := http.ListenAndServe(":8080", r)
